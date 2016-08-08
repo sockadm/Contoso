4ef59a38627b9f003c713a2a81a416e2ac59d8fb
Sandro Oliveira <sock.adm@gmail.com>
2016-08-07 22:47:06 -0300

    1 commit - nao esta funcionando entegracao com o bd

---
+++ Contoso/ContosoWeb/Models/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ContosoWeb.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)] // permite que crie a chave primaria no lugar da auto-geração.
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+
+    }
+}
---
+++ Contoso/ContosoWeb/Models/Enrollment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoWeb.Models
+{
+
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        public Grade? Grade { get; set; } // o ? significa que a classe pode ser nullable.
+
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
